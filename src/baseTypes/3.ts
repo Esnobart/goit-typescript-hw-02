f66cbda582b2f2247ffb0cb9e446b39534339f1f
Esnobart <offrichterrr@gmail.com>
2024-03-16 15:02:54 +0000

    update

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,10 @@
 let some:unknown;
 some = 'Text';
-let str: unknown;
-str = some;
+let str: string;
+if (typeof some === 'string') {
+    str = some;
+} else {
+    
+}
 
 export {};
--- src/baseTypes/6.ts
+++ src/baseTypes/6.ts
@@ -2,23 +2,21 @@
   Як ви вкажете типи для аргументів і значень цих функцій, що повертаються?
 */
 
-function showMessage(message) {
+function showMessage(message: string): void {
   console.log(message);
 }
 
-let message: any = showMessage('good luck');
+let message: void = showMessage('good luck');
 
-function calc(num1, num2) {
+function calc(num1: number, num2: number): number {
   return num1 + num2;
 }
 
 let number: number = calc(7, 2);
 
-function customError() {
+function customError(): never {
   throw new Error('Error');
 }
-
-let error: void = customError();
 
 
 export {};
